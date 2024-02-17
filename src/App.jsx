6894651564c6e7c1578f8bac99e21808597c37b5
dimovv23 <ggdimovv@gmail.com>
2024-02-17 15:03:00 +0200

    Added a 'ShoeCard' component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Nav from "./components/Nav.jsx";
 const App = () => (
   <main className="realtive">
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
